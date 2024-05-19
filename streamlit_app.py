a84855e738d6cac0c7a9da24be84973ff3b62117
Rafael Eugenio <166950652+AlpharafGitHub@users.noreply.github.com>
2024-05-19 18:39:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,37 @@ import tensorflow as tf
 from tensorflow.keras import models
 from PIL import Image
 import numpy as np
+import requests
+import os
+
+# Function to download the model file
+def download_model_file(url, file_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        return True
+    else:
+        return False
+
+# Download the model file
+model_url = "https://github.com/AlpharafGitHub/Cats-and-Dogs-Hybrid-Classifier/raw/main/cats_and_dogs_small_333.h5"
+model_path = "cats_and_dogs_small_333.h5"
+if not os.path.exists(model_path):
+    st.write("Downloading model file...")
+    if download_model_file(model_url, model_path):
+        st.write("Model file downloaded successfully.")
+    else:
+        st.write("Failed to download model file. Please check the URL.")
 
 # Load the model
-model_path = "https://github.com/AlpharafGitHub/Cats-and-Dogs-Hybrid-Classifier/raw/main/cats_and_dogs_small_333.h5"
 model = tf.keras.models.load_model(model_path)
 
-# Function to preprocess the image
+# Function to preprocess the image (same as before)
 def preprocess_image(image):
-    img = image.resize((28, 28))  # Resize image to match model's expected sizing
-    img = np.array(img) / 255.0  # Normalize pixel values to range [0, 1]
-    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    img = image.resize((28, 28))  
+    img = np.array(img) / 255.0  
+    img = np.expand_dims(img, axis=0)  
     return img
 
 def main():
@@ -21,14 +42,11 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Display the uploaded image
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.', use_column_width=True)
 
-        # Preprocess the image
         img_array = preprocess_image(image)
 
-        # Make prediction
         prediction = model.predict(img_array)
         if prediction[0][0] > 0.5:
             st.write("Prediction: Dog")
