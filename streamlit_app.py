30618272dc37b3ed1e6c39359d654c4fadca8a17
Rafael Eugenio <166950652+AlpharafGitHub@users.noreply.github.com>
2024-05-19 18:58:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,16 @@ import tensorflow as tf
 from tensorflow.keras import models
 from PIL import Image
 import numpy as np
-import requests
-import os
 
-# Function to download the model file (same as before)
 # Load the model
 model_path = "https://github.com/AlpharafGitHub/Cats-and-Dogs-Hybrid-Classifier/raw/main/cats_and_dogs_small_333.h5"
-model = tf.keras.models.load_model(model_path, compile=False)
+model = tf.keras.models.load_model(model_path)
 
-# Function to preprocess the image (same as before)
+# Function to preprocess the image
 def preprocess_image(image):
-    img = image.resize((150, 150))  
-    img = np.array(img) / 255.0  
-    img = np.expand_dims(img, axis=0)  
+    img = image.resize((28, 28))  # Resize image to match model's expected sizing
+    img = np.array(img) / 255.0  # Normalize pixel values to range [0, 1]
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
     return img
 
 def main():
@@ -24,11 +21,14 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
+        # Display the uploaded image
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.', use_column_width=True)
 
+        # Preprocess the image
         img_array = preprocess_image(image)
 
+        # Make prediction
         prediction = model.predict(img_array)
         if prediction[0][0] > 0.5:
             st.write("Prediction: Dog")
